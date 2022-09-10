c96c1aab462315c0d70ff960b3385d4b7a32b9f7
jrcoforge <113238291+jrcoforge@users.noreply.github.com>
2022-09-10 16:11:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,2 @@
-import io
-
-output = io.StringIO()
-output.write('First line.\n')
-print('Second line.', file=output)
-
-# Retrieve file contents -- this will be
-# 'First line.\nSecond line.\n'
-contents = output.getvalue()
-
-# Close object and discard memory buffer --
-# .getvalue() will now raise an exception.
-output.close()
+import streamlit
+streamlit.title("My Parents New Healthy Diner")
