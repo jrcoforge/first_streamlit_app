d47695d90fb3e31b201d7d5bf7af569dc3c3422c
jrcoforge <113238291+jrcoforge@users.noreply.github.com>
2022-09-10 16:07:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import io
 
+output = io.StringIO()
+output.write('First line.\n')
+print('Second line.', file=output)
+
+# Retrieve file contents -- this will be
+# 'First line.\nSecond line.\n'
+contents = output.getvalue()
+
+# Close object and discard memory buffer --
+# .getvalue() will now raise an exception.
+output.close()
