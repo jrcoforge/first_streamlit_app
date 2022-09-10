5b4f626c2c3fd06f3b90d466e29105d722513e6d
jrcoforge <113238291+jrcoforge@users.noreply.github.com>
2022-09-10 16:18:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
+
 streamlit.title("My Parents New Healthy Diner")
+
+sreamlit.header('Breakfast Menu')
+sreamlit.text('Omega 3 & Blueberry Oatmeal')
+
+sreamlit.text('Kale, Spinach & Rocket Smoothie')
+sreamlt.text('Hard-Boiled Free-Range Egg' )
+
