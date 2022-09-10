1ce7b0224f73e8bd6720f88557689e040d74e15b
jrcoforge <113238291+jrcoforge@users.noreply.github.com>
2022-09-10 16:19:53 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ sreamlit.header('Breakfast Menu')
 sreamlit.text('Omega 3 & Blueberry Oatmeal')
 
 sreamlit.text('Kale, Spinach & Rocket Smoothie')
-sreamlt.text('Hard-Boiled Free-Range Egg' )
+sreamlit.text('Hard-Boiled Free-Range Egg' )
 
